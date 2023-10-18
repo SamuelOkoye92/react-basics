1d7053b7cf00c9116b2e1acc48918e3ae0770eb9
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-10-18 22:08:33 +0200

    adjustment

--- React Practice.jsx
+++ React Practice.jsx
@@ -171,24 +171,29 @@ export default function Square() {
 
 // exempt
 
-export default function Board() {
+function square () {
+    return <button className="square">1</button>
+  }
+  
+  export default function Board() {
     return (
       <>
       <div className = "board-row">
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
+      <square />
+      <square />
+      <square />
        </div>
       <div className = "board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
+      <square />
+      <square />
+      <square />
       </div>
       <div className = "board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+      <square />
+      <square />
+      <square />
       </div>
       </>
     ); 
   }
+  
